f75a32b29e5705226c43e8a5f5a5f775335d9174
Ronald Weigand <r-weigand@onu.edu>
2024-10-24 09:34:21 -0400

    Completed countVowel function

--- Source.cpp
+++ Source.cpp
@@ -18,3 +18,15 @@ int main() {
 int countCharacter(string line) {
     return line.length();
 }
+int countVowel(string line) {
+    int vowels = 0;
+    for (int i = 0; i < line.length(); i++) {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U') {
+            vowels++;
+        }
+    }
+    return vowels;
+}
